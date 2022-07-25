044688b7865e51a6d17bdabb4b81b523e7f29647
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-07-25 16:36:39 +0600

    modify projects

--- src/router/Projects.vue
+++ src/router/Projects.vue
@@ -3,56 +3,105 @@
     <h3><strong>Projects</strong></h3>
     <br>
     <div>
-      <h4> Web-Based Projects </h4>
+      <h4> Machine Learning Projects: </h4>
       <ul>
-        <li><strong> Vue-folio </strong></li>
+        <li><strong> AI COACH (iOS) </strong></li>
         <div>
           <strong> Project Details: </strong>
-            A basic portfolio website built with Vue.js. The concept 'Single Page Application' (SPA) was implemented
-            while playing around with this progressive JavaScript framework. Being a static website, the scope was 
-            limited. But you could find out that your browser does not require any page reloading on this website.
+          AI COACH is a virtual therapist which can guide patients for the correct movements of an exercise in real-time.
+          I have built this application from scratch and worked on a range of different sub-projects, i.e. Design UI, Handle
+          API call, Design app flow, Manage software architecture (MVVM), Deploy Pose Detection models, develop and deploy
+          Gesture Recognition models, Real-time Frame Processing, Implement graphical objects (i.e. angle, distance, texts, etc.)
+          based on the real-time movements of a person, Manage AI COACH app in Apple's TestFlight.
           <br>
-          <strong> Timeline: </strong> October 2020.<br>
-          <strong> Language and Tools: </strong> Vue CLI, vue Router, HTML, CSS, and Bootstrap.<br>
-          <strong> GitHub Repository: </strong> 
-          <a href="https://github.com/Sabit-Ahmed/Sabit-Ahmed.info">https://github.com/Sabit-Ahmed/Sabit-Ahmed.info</a>
+          <strong> Demo</strong>
+          <strong> Timeline: </strong> January 2022 – Present.<br>
+          <strong> Language and Tools: </strong> Python, Swift, Xcode, Tensorflow, PyTorch, Core-ML, Core-Graphics, etc.<br>
+          <a href="https://drive.google.com/file/d/1fge5FFAuyR7shyp7ZTsiE7K4ZPKXOKnf/view" target="_blank"><strong> See Demo application </strong></a>
         </div>
         <br>
-        <li><strong> Containerized CRUD API </strong></li>
+        <li><strong> AI COACH (Android) </strong></li>
         <div>
           <strong> Project Details: </strong>
-            A simple containerized CRUD application based on the Representational State Transfer (REST). 
-            The first version included one single container and the later version included two separate 
-            containers (i.e. API and database) with a shared network.
+          I have implemented The android version of AI COACH to enhance the functionality of range-of-motion (ROM) of a person.
+          Here, Background Segmentation model has been used to calculate the virtual heights and widths of different body parts,
+          such as, shoulder to shoulder distance, toe to head distances, etc.
           <br>
-          <strong> Timeline: </strong> October 2020.<br>
-          <strong> Language and Tools: </strong> Docker, Flask, Python, HTML, CSS, and MySQL.<br>
-          <strong> GitHub Repository: </strong> 
-          <a href="https://github.com/Sabit-Ahmed/Containerized-CRUDpy">https://github.com/Sabit-Ahmed/Containerized-CRUDpy</a>
+          <strong> Timeline: </strong> September 2021 – December 2021.<br>
+          <strong> Language and Tools: </strong> Kotlin, Android Studio, Mediapipe, ML-Kit, etc.<br>
+          <strong> GitHub Repository: </strong>
+          <a href="https://github.com/Sabit-Ahmed/virtual-fitness-instructor">https://github.com/Sabit-Ahmed/virtual-fitness-instructor</a>
+          <a href="https://drive.google.com/file/d/1CT8Fc_d48HQsnx7SJ6JqWX1O87Xyzp7k/view" target="_blank"><strong> See Demo </strong></a>
         </div>
         <br>
-        <li><strong> Web Scraping API </strong></li>
+        <li><strong> Hand Detection </strong></li>
         <div>
           <strong> Project Details: </strong>
-            A simple Flask application where customer reviews can be scraped based on the query product. 
-            In addition to that, another scraping application was constructed for scraping the query images from google.
+          The hand detection is a pre-requisite for detecting different hand gestures of a patient. Resnet50, MobileNet_V2,
+          single shot detector (SSD) models have been combined and trained via transfer learning. Combination of COCO Hand,
+          TV-Hand, and Ego-Hand datasets has been used to train the model.
           <br>
-          <strong> Timeline: </strong> September 2020.<br>
-          <strong> Language and Tools: </strong> Flask, Python, HTML, CSS, and MongoDB.<br>
-          <strong> GitHub Repository: </strong> 
-          <a href="https://github.com/Sabit-Ahmed/Python-Practice">https://github.com/Sabit-Ahmed/Python-Practice</a>
+          <strong> Timeline: </strong> September 2021 – December 2021.<br>
+          <strong> Language and Tools: </strong> Kotlin, Android Studio, Mediapipe, ML-Kit, etc.<br>
+          <strong> GitHub Repository: </strong>
+          <a href="https://github.com/Sabit-Ahmed/HandDetection">https://github.com/Sabit-Ahmed/HandDetection</a>
+          <a href="https://drive.google.com/file/d/1d0i8vLZllN5gQmqPBxgJaNx2EDDEYyea/view?usp=sharing" target="_blank"><strong> See Demo </strong></a>
         </div>
+      </ul>
+    </div>
+    <br> <br>
+    <div>
+      <h4> Image Processing: </h4>
+      <div>
+        <strong> Project Details: </strong>
+        Various types of image filtering techniques were utilized for image smoothing, image sharpening,
+        and many other operations.
         <br>
-        <li><strong> A Service Providing Application </strong></li>
+        <strong> Timeline: </strong> August 2019 – November 2019.<br>
+        <strong> Language and Tools: </strong> MATLAB.<br>
+        <strong> GitHub Repository: </strong>
+        <a href="https://github.com/Sabit-Ahmed/Image-Processing-Matlab">https://github.com/Sabit-Ahmed/Image-Processing-Matlab</a>
+      </div>
+    </div>
+    <br><br>
+    <div>
+      <h4> Research Projects: </h4>
+      <ul>
+        <li><strong> Post-Translational Modifications </strong></li>
         <div>
           <strong> Project Details: </strong>
-            A service providing application. Both customers and workers could register. Workers would be ready for 
-            the delivery of the desired service ordered by the customers.
+          This project aims to identify protein's post-translational modifications both individually and
+          simultaneously with higher efficacy. We have developed three single label predictors such as
+          Formyl_Pred, predPhogly-Site, and PredNitro which correspond to Formylation, Phosphoglycerylation,
+          and Nitrotyrosine sites prediction separately, and two multi-label predictors, such as,
+          predML-Site, and iMul-kSite for predicting Acetylation, Crotonylation, Methylation, Succinylation,
+          and glutarylation sites simultaneously. More information could be found in the publications section.
           <br>
-          <strong> Timeline: </strong> March 2018 – August 2018.<br>
-          <strong> Language and Tools: </strong> PHP, HTML, CSS, and MySQL.<br>
-          <strong> GitHub Repository: </strong> 
-          <a href="https://github.com/Sabit-Ahmed/3-1-project">https://github.com/Sabit-Ahmed/3-1-project</a>
+          <strong> Timeline: </strong> August 2019 – Present.<br>
+          <strong> Language and Tools: </strong> MATLAB, Python, Scikit-learn, Django, HTML, and CSS.<br>
+          <strong> Web-Servers: </strong>
+          <ul>
+            <li>
+              <strong> predML-Site: </strong>
+              <a href="http://103.99.176.239/predML-Site">http://103.99.176.239/predML-Site</a>
+            </li>
+            <li>
+              <strong> predPhogly-Site: </strong>
+              <a href="http://103.99.176.239/predML-Site">http://103.99.176.239/predPhogly-Site</a>
+            </li>
+            <li>
+              <strong> iMul-kSite: </strong>
+              <a href="http://103.99.176.239/iMul-kSite">http://103.99.176.239/iMul-kSite</a>
+            </li>
+            <li>
+              <strong> PredNitro: </strong>
+              <a href="http://103.99.176.239/PredNitro">http://103.99.176.239/PredNitro</a>
+            </li>
+            <li>
+              <strong> ResearchGate: </strong>
+              <a href="https://www.researchgate.net/project/Post-translational-Modifications">https://www.researchgate.net/project/Post-translational-Modifications</a>
+            </li>
+          </ul>
         </div>
       </ul>
     </div>
@@ -76,71 +125,58 @@
           <br>
           <a href="https://github.com/Sabit-Ahmed/React-Native-Projects/tree/main/rn9app" target="_blank"><strong> See Demo application </strong></a>
         </div>
-        <br>
-        <li><strong> Learn Therapist (Android and iOS) </strong></li>
-        <div>
-          <strong> Project Details: </strong>
-            Learn Therapist is a new integration project of EMMA - LPT. It is a virtual therapist 
-            which can guide patients for the correct movements of an exercise. This is an ongoing project.
-          <br>
-          <strong> Timeline: </strong> August 2021 – Present.<br>
-          <strong> Language and Tools: </strong> Kotlin, Swift, Android Studio, Xcode.<br>
-        </div>
         </ul>
     </div>
-    <br><br>
+    <br> <br>
     <div>
-      <h4> Image Processing: </h4>
-        <div>
-          <strong> Project Details: </strong>
-            Various types of image filtering techniques were utilized for image smoothing, image sharpening, 
-            and many other operations.
-          <br>
-          <strong> Timeline: </strong> August 2019 – November 2019.<br>
-          <strong> Language and Tools: </strong> MATLAB.<br>
-          <strong> GitHub Repository: </strong> 
-          <a href="https://github.com/Sabit-Ahmed/Image-Processing-Matlab">https://github.com/Sabit-Ahmed/Image-Processing-Matlab</a>
-        </div>
-    </div>
-    <br><br>
-    <div>
-      <h4> Research Projects </h4>
+      <h4> Other Projects: </h4>
       <ul>
-        <li><strong> Post-Translational Modifications </strong></li>
+        <li><strong> Vue-folio </strong></li>
         <div>
           <strong> Project Details: </strong>
-            This project aims to identify protein's post-translational modifications both individually and 
-            simultaneously with higher efficacy. We have developed three single label predictors such as 
-            Formyl_Pred, predPhogly-Site, and PredNitro which correspond to Formylation, Phosphoglycerylation,
-             and Nitrotyrosine sites prediction separately, and two multi-label predictors, such as, 
-             predML-Site, and iMul-kSite for predicting Acetylation, Crotonylation, Methylation, Succinylation,
-              and glutarylation sites simultaneously. More information could be found in the publications section.
+          A basic portfolio website built with Vue.js. The concept 'Single Page Application' (SPA) was implemented
+          while playing around with this progressive JavaScript framework. Being a static website, the scope was
+          limited. But you could find out that your browser does not require any page reloading on this website.
           <br>
-          <strong> Timeline: </strong> August 2019 – Present.<br>
-          <strong> Language and Tools: </strong> MATLAB, Python, Scikit-learn, Django, HTML, and CSS.<br>
-          <strong> Web-Servers: </strong> 
-          <ul>
-            <li>
-              <strong> predML-Site: </strong>
-              <a href="http://103.99.176.239/predML-Site">http://103.99.176.239/predML-Site</a>
-            </li>
-            <li>
-              <strong> predPhogly-Site: </strong>
-              <a href="http://103.99.176.239/predML-Site">http://103.99.176.239/predPhogly-Site</a>
-            </li>
-            <li>
-              <strong> iMul-kSite: </strong>
-              <a href="http://103.99.176.239/iMul-kSite">http://103.99.176.239/iMul-kSite</a>
-            </li>
-            <li>
-              <strong> PredNitro: </strong>
-              <a href="http://103.99.176.239/PredNitro">http://103.99.176.239/PredNitro</a>
-            </li>
-            <li>
-              <strong> ResearchGate: </strong>
-              <a href="https://www.researchgate.net/project/Post-translational-Modifications">https://www.researchgate.net/project/Post-translational-Modifications</a>
-            </li>
-          </ul>
+          <strong> Timeline: </strong> October 2020.<br>
+          <strong> Language and Tools: </strong> Vue CLI, vue Router, HTML, CSS, and Bootstrap.<br>
+          <strong> GitHub Repository: </strong>
+          <a href="https://github.com/Sabit-Ahmed/Sabit-Ahmed.info">https://github.com/Sabit-Ahmed/Sabit-Ahmed.info</a>
+        </div>
+        <br>
+        <li><strong> Web Scraping API </strong></li>
+        <div>
+          <strong> Project Details: </strong>
+          A simple Flask application where customer reviews can be scraped based on the query product.
+          In addition to that, another scraping application was constructed for scraping the query images from google.
+          <br>
+          <strong> Timeline: </strong> September 2020.<br>
+          <strong> Language and Tools: </strong> Flask, Python, HTML, CSS, and MongoDB.<br>
+          <strong> GitHub Repository: </strong>
+          <a href="https://github.com/Sabit-Ahmed/Python-Practice">https://github.com/Sabit-Ahmed/Python-Practice</a>
+        </div>
+        <br>
+        <li><strong> Containerized CRUD API </strong></li>
+        <div>
+          <strong> Project Details: </strong>
+          A simple containerized CRUD application based on the Representational State Transfer (REST).
+          The first version included one single container and the later version included two separate
+          containers (i.e. API and database) with a shared network.
+          <br>
+          <strong> Timeline: </strong> October 2020.<br>
+          <strong> Language and Tools: </strong> Docker, Flask, Python, HTML, CSS, and MySQL.<br>
+          <strong> GitHub Repository: </strong>
+          <a href="https://github.com/Sabit-Ahmed/Containerized-CRUDpy">https://github.com/Sabit-Ahmed/Containerized-CRUDpy</a>
+        </div>
+        <br>
+        <li><strong> Problem Solving </strong></li>
+        <div>
+          <strong> Project Details: </strong>
+          This repository contains solved problems in various online judge platforms i.e. LeetCdoe, HackerRank, CodeForces, etc.
+          <br>
+          <strong> Language and Tools: </strong> Python, Swift, C++.<br>
+          <strong> GitHub Repository: </strong>
+          <a href="https://github.com/Sabit-Ahmed/Problem-Solving">https://github.com/Sabit-Ahmed/Problem-Solving</a>
         </div>
       </ul>
     </div>
